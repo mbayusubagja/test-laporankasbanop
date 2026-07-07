ef9889b45575da4eb3603228413edbecb4987a74
Muhammad Bayu Subagja <66833650+mbayusubagja@users.noreply.github.com>
2026-07-07 07:15:44 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
 const API =
-"https://script.google.com/macros/s/AKfycbyiJx1lgCXpt0vUZ3KwL_tq7_lBPIkkFZMXVtLTuRJ1gnH0pMzsJVLip-4dHeYPOafnFQ/exec";
+"https://script.google.com/macros/s/AKfycbyAc47_IG1oU_Vg5nid3NCsCyd8_AdMF6_gcX7col75KRjeNuvTeB9bIMfKqfcVTr1kcA/exec";
