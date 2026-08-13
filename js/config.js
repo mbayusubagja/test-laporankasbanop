03c145b38a8db2303b932cc6fa613cb74d7c80a7
Muhammad Bayu Subagja <66833650+mbayusubagja@users.noreply.github.com>
2026-08-13 13:49:56 +0700

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -1,3 +1,2 @@
 const API =
-//"https://script.google.com/macros/s/AKfycbyiJx1lgCXpt0vUZ3KwL_tq7_lBPIkkFZMXVtLTuRJ1gnH0pMzsJVLip-4dHeYPOafnFQ/exec";
-"https://script.google.com/macros/s/AKfycbyAc47_IG1oU_Vg5nid3NCsCyd8_AdMF6_gcX7col75KRjeNuvTeB9bIMfKqfcVTr1kcA/exec";
+"https://script.google.com/macros/s/AKfycbwYPS0_Ju4BsKHvKXs4KhD7RkLaVBeNC7wzfBtwhMUejJMcWKkV71W-HjOytY0tIy_i/exec";
